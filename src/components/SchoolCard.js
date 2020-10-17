574289db7363097f5ae7567109ab99457dfec7bc
Izagma Alonso <ialonso@manhattanbridgeshs.org>
2020-10-17 17:21:36 +0000

    NY lab, extension 1 included

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school.ENTITY_NAME}</h5>
         <div className="card-body">
           <h5 className="card-title">
             <span className="badge">2017-18</span>
@@ -15,13 +15,14 @@ function SchoolCard() {
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
             <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
-            <li className="list-group-item">3-8 Math Assessment Data</li>
+            <li className="list-group-item">Number Of White Students {props.school.NUM_WHITE}</li>
+            <li className="list-group-item">Number of ELLs {props.school.NUM_ELL}</li>
+            <li className="list-group-item">Number of Immigrants {props.school.NUM_MIGRANT}</li>
           </ul>
           <span className="badge badge-header">School Data</span>
           <ul className="list-group">
-            <li className="list-group-item">School Report Card</li>
-            <li className="list-group-item">Student and Educator Report</li>
+            <li className="list-group-item">Year Started {props.school.YEAR}</li>
+            {/* <li className="list-group-item">Student and Educator Report</li> */}
           </ul>
         </div>
       </div>
